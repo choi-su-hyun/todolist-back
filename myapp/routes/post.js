8f3e5a4a22c3f067916f5aba9137d79213d4bed9
최수현 <choi45769@gmail.com>
2023-02-11 17:10:21 +0900

    리스트 관련하여 생성, 수정, 삭제 기능 구현

--- myapp/routes/post.js
+++ myapp/routes/post.js
@@ -17,4 +17,61 @@ router.get('/', function(req, res){
     })
 })
 
+// create api
+router.post('/create-process', function(req, res) {
+    const post = req.body;
+    console.log(post)
+    db.query(`INSERT INTO content_list(title, paragraph, created, author_id) VALUES(?, ?, now(), ?)`,[post.todoTitle, post.todoDetail, req.user.id], function(err, result){
+        if(err) {
+            res.status(500).json({
+                message: 'DB_ERROR'
+            })
+        }
+        res.send('CREATE_SUCCESS')
+    })
+})
+
+// delete api
+router.delete('/delete/:id', function(req, res) {
+    const willDeletePost = req.params.id
+    db.query(`DELETE FROM content_list WHERE id=?`,[willDeletePost], function(err, result) {
+        if(err) {
+            res.status(500).json({
+                message: 'DB_ERROR'
+            })
+        }
+        res.send('DELETE_SUCCESS');
+    })
+})
+
+// update를 위한 해당 글 내용 가져오기 api
+router.get('/edit/:id', function(req, res) {
+    db.query(`SELECT title, paragraph FROM content_list WHERE id=?`,[req.params.id], function(err, row) {
+        if(err) {
+            res.status(500).json({
+                message: 'DB_ERROR'
+            })
+        }
+        res.status(200).json({
+            message: 'BEFORE_UPDATE_DATA_SUCCESS',
+            updateData: row[0],
+        })
+    })
+})
+
+// update api
+router.put('/edit/:id/update_process', function(req, res) {
+    const post = req.body
+    db.query(`UPDATE content_list SET title=?, paragraph=? WHERE id=?`,[post.todoTitle, post.todoDetail, req.params.id], function(err, row) {
+        if(err) {
+            res.status(500).json({
+                message: 'DB_ERROR'
+            })
+        }
+        res.status(200).json({
+            message: 'UPDATE_SUCCESS',
+        })
+    })
+})
+
 module.exports = router;
